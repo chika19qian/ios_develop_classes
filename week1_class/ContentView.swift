8199c49e7f21284f6d2de57b64aa48df70ba411a
chika19qian <chika5747@g.ecc.u-tokyo.ac.jp>
2024-11-10 19:26:21 +0900

    togglable isFaceUp

--- week1_class/ContentView.swift
+++ week1_class/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
             CardView()
             CardView()
             CardView()
@@ -22,22 +22,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = true
     
     var body: some View {
-        
-        ZStack (content: {
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 3)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 3)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
-                    
+                base.fill()
             }
-        })
+        }
+        .onTapGesture{
+            isFaceUp.toggle()
+        }
     }
 }
 
